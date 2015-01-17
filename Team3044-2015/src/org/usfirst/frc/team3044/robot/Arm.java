48aab4e00761b8d728e6d933b11b250a86c8bfa1
Joey <joeylyon1@gmail.com>
2015-01-17 11:56:58 -0500

    Modified Arm and Robot Classes for skeleton setup

--- Team3044-2015/src/org/usfirst/frc/team3044/robot/Arm.java
+++ Team3044-2015/src/org/usfirst/frc/team3044/robot/Arm.java
@@ -1,8 +1,26 @@
 package org.usfirst.frc.team3044.robot;
 
+import org.usfirst.frc.team3044.utils.Utilities;
+
 public class Arm {
 	
-	public void init(){
+	public void robotInit(){
+		
+	}
+	
+	public void teleopInit(){
+		
+	}
+	
+	public void autoInit(){
+		
+	}
+	
+	public void disabled(){
+		
+	}
+	
+	public void armPeriodic(){
 		
 	}
 
