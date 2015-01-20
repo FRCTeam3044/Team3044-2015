946fe66c838d9060fffc73d53df428cd661b026d
Joey <joeylyon1@gmail.com>
2015-01-19 20:33:15 -0500

    updated forklift to add skeleton functions

--- Team3044-2015/src/org/usfirst/frc/team3044/robot/Forklift.java
+++ Team3044-2015/src/org/usfirst/frc/team3044/robot/Forklift.java
@@ -1,8 +1,27 @@
 package org.usfirst.frc.team3044.robot;
 
+import org.usfirst.frc.team3044.utils.Components;
+
 public class Forklift {
 	
-	public void init(){
+	public void robotInit(){
+		
+	}
+	
+	public void teleopInit(){
+		
+	}
+	
+	public void autoInit(){
+		
+	}
+	
+	public void disabled(){
+		
+	}
+	
+	public void forkliftPeriodic(){
+		
 		
 	}
 
