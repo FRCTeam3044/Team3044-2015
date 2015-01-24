2d3b46edc6a4974efef673dd9a0c79bf74fd00eb
DAVIDAgustin <DAVIDAgustin@David-PC>
2015-01-23 19:01:53 -0500

    Fixed Arm Code After Component Changes

--- Team3044-2015/src/org/usfirst/frc/team3044/robot/Arm.java
+++ Team3044-2015/src/org/usfirst/frc/team3044/robot/Arm.java
@@ -19,64 +19,61 @@ final int Middle = 3;
 final int Out =4;
 final int MovingIn=5;
 int ArmState = IN;
+Components components = Components.getInstance();
 
 	public void robotInit(){
-		
 	}
-	
 	public void teleopInit(){
 	}	
 	public void autoInit(){
 	}
 	public void disabled(){
-		
-	
 	}
 	public void armPeriodic(){
 		
 		ArmButtonIn1 = ArmJoy.getRawButton(1);
 		ArmButtonOut1 = ArmJoy.getRawButton(Components.ARM_OUT);
 		PneumaticsButton1 = ArmJoy.getRawButton(2);
-		int ArmState = Components.ARM_IN;
+		int ArmState = IN;
 		
 	switch(ArmState){
     	
 	case IN:
     		if (ArmButtonOut1 = true){
-    			if(!Components.ArmExtended.get()){
-    				Components.armMotor.set(1);
+    			if(!components.ArmExtended.get()){
+    				components.armMotor.set(1);
     				ArmState = MovingOut;
        			}
     		}
     	case Out:
     		if (ArmButtonIn1 = true){
-    			if(!Components.ArmRetracted.get()){
-    				Components.armMotor.set(-1);
+    			if(!components.ArmRetracted.get()){
+    				components.armMotor.set(-1);
     				ArmState = MovingIn;
        			}
     		}
     	case MovingOut:
     		if (ArmButtonOut1 = false){
-    			if(!Components.ArmExtended.get()){
-    				Components.armMotor.set(0);
+    			if(!components.ArmExtended.get()){
+    				components.armMotor.set(0);
     				ArmState = Middle;
     			}
     		}
     	case MovingIn:
     		if (ArmButtonIn1 = false){
-    			if(!Components.ArmRetracted.get()){
-    				Components.armMotor.set(0);
+    			if(!components.ArmRetracted.get()){
+    				components.armMotor.set(0);
     				ArmState = Middle;
     			}
     		}
     	case Middle:
     		if (ArmButtonOut1 = true){
-    			if(!Components.ArmExtended.get()){
-    				Components.armMotor.set(1);
+    			if(!components.ArmExtended.get()){
+    				components.armMotor.set(1);
     				ArmState = MovingOut;
     		}else if (ArmButtonIn1 = true){
-    			if(!Components.ArmRetracted.get()){
-    				Components.armMotor.set(-1);
+    			if(!components.ArmRetracted.get()){
+    				components.armMotor.set(-1);
     				ArmState = MovingIn;
     				
     			
