7dc3f84b5d8d5e04ce012cd4f9c247c0f010048d
Joey <joeylyon1@gmail.com>
2015-02-03 18:52:32 -0500

    Added initializers

--- Team3044-2015/src/org/usfirst/frc/team3044/utils/Components.java
+++ Team3044-2015/src/org/usfirst/frc/team3044/utils/Components.java
@@ -26,10 +26,10 @@ public class Components {
 	 ex. Sensors, motor controllers, etc,
 	 */  
 	
-	public CANJaguar frontRightDriveRot;
-	public CANJaguar frontLeftDriveRot;
-	public CANJaguar backRightDriveRot;
-	public CANJaguar backLeftDriveRot;
+	public CANTalon frontRightDriveRot;
+	public CANTalon frontLeftDriveRot;
+	public CANTalon backRightDriveRot;
+	public CANTalon backLeftDriveRot;
 	
 	public CANJaguar frontRightDrive;
 	public CANJaguar frontLeftDrive;
@@ -47,7 +47,7 @@ public class Components {
 	public DigitalInput forkliftUp = new DigitalInput(0);
 	public DigitalInput forkliftDown = new DigitalInput(1);
 	
-	//Happy Sweet 16 Ryan! -joey 
+	
 	public Solenoid forkliftClamp2 = new Solenoid(0);
 	public Solenoid armSolenoid = new Solenoid(1);
 	public Solenoid forkliftClamp = new Solenoid(3);
@@ -55,11 +55,11 @@ public class Components {
 	public DigitalInput ArmExtended = new DigitalInput(2);
 	public DigitalInput ArmRetracted = new DigitalInput(3);
 	
-	public DigitalInput ArmSliderOut;
-	public DigitalInput ArmSliderIn;
+	public DigitalInput ArmSliderOut = new DigitalInput(1);
+	public DigitalInput ArmSliderIn = new DigitalInput(2);
 	
-	public Encoder encoderSlider;
-	public Encoder encoderArm;
+	public Encoder encoderSlider = new Encoder(0,0);
+	public Encoder encoderArm = new Encoder(0,0);
 	
 	public final int ARM_OUT_BUTTON = 1;
 	public final int ARM_IN_BUTTON = 3;
@@ -89,17 +89,17 @@ public class Components {
 	public Encoder rotEncoderFL = new Encoder(1, 12);
 	public Encoder rotEncoderBL = new Encoder(1, 13);
 	
-	public DigitalInput proximityFR;
-	public DigitalInput proximityBR;
-	public DigitalInput proximityAbsFL;
-	public DigitalInput proximityAbsBL;	
+	public DigitalInput proximityFR = new DigitalInput(0);
+	public DigitalInput proximityBR = new DigitalInput(0);
+	public DigitalInput proximityAbsFL = new DigitalInput(0);
+	public DigitalInput proximityAbsBL = new DigitalInput(0);	
 	
 	
 	public void init(){
-		frontRightDriveRot = new CANJaguar(0);
-		frontLeftDriveRot = new CANJaguar(0);
-		backRightDriveRot = new CANJaguar(0);
-		backLeftDriveRot = new CANJaguar(0);
+		frontRightDriveRot = new CANTalon(0);
+		frontLeftDriveRot = new CANTalon(0);
+		backRightDriveRot = new CANTalon(0);
+		backLeftDriveRot = new CANTalon(0);
 		
 		frontRightDrive = new CANJaguar(0);
 		frontLeftDrive = new CANJaguar(0);
