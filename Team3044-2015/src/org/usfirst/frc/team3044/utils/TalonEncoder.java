235425acffc382bbb5430a25f8fb63b6d105162c
Joey <joeylyon1@gmail.com>
2015-02-07 10:53:31 -0500

    Added helper file for working with encoders plugged into talons

---
+++ Team3044-2015/src/org/usfirst/frc/team3044/utils/TalonEncoder.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team3044.utils;
+
+import edu.wpi.first.wpilibj.CANTalon;
+
+public class TalonEncoder {
+	
+	private CANTalon talonInstance;
+	
+	//Is this even worth implementing
+	
+	public TalonEncoder(CANTalon talonInstance){
+		this.talonInstance = talonInstance;
+	}
+	
+	public void reset(){
+		
+	}
+
+}
