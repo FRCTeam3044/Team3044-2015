204be5ff949ac4adf025a31ce71376db2291ad6f
jlyon1 <jlyon1@users.noreply.github.com>
2016-01-23 15:29:00 -0500

    Create AnalogEncoderWrapper.java

---
+++ AnalogEncoderWrapper.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team3044.utils;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+public class AnalogEncoderWrapper {
+	
+	private AnalogInput encoderInstance;
+	private double oldValue = 0;
+	private double value = 0;
+	private final double MAX_VAL = 255;
+	double distance = 0;
+	
+	public AnalogEncoderWrapper(AnalogInput encoderInstance){
+		this.encoderInstance = encoderInstance;
+		
+	}
+	
+	public void step(){
+		value = encoderInstance.getVoltage();
+		if(value - oldValue > 4){
+			distance += 1;
+		}
+		else if(value - oldValue < -4){
+			distance -= 1;
+		}
+		oldValue  = encoderInstance.getVoltage();
+		
+		
+	}
+	
+	public double getDistance(){
+		return distance;
+	}
+	
+	public void reset(){
+		this.distance = 0;
+	}
+	
+	public void reset(double value){
+		this.distance = value;
+	}
+
+}
